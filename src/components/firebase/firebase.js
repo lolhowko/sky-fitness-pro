6dda1f897766bae3759f542cc1742e2fb1a02317
Lana <lolhowko@gmail.com>
2024-03-24 18:13:31 +0300

    add firebase

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,18 +8,18 @@ import "firebase/compat/firestore";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC07W8gEHPHqQ1WLX0f8RnkyZ1kP1IpH8s",
-    authDomain: "fitness-pro-604a8.firebaseapp.com",
-    databaseURL: "https://fitness-pro-604a8-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "fitness-pro-604a8",
-    storageBucket: "fitness-pro-604a8.appspot.com",
-    messagingSenderId: "522804164082",
-    appId: "1:522804164082:web:56728020ec99022912a76b"
-};
-
+  apiKey: 'AIzaSyDek6C_3ZVDfbsr0-PXenxWO9JjN_Whm2Q',
+  authDomain: 'sky-fitness-pro-3a520.firebaseapp.com',
+  databaseURL:
+    'https://sky-fitness-pro-3a520-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'sky-fitness-pro-3a520',
+  storageBucket: 'sky-fitness-pro-3a520.appspot.com',
+  messagingSenderId: '103077240446',
+  appId: '1:103077240446:web:c0df83f0940d41ad62022f',
+}
 
 // Initialize Firebase
-export const app = firebase.initializeApp(firebaseConfig);
+export const app = firebase.initializeApp(firebaseConfig)
 // console.log(firebase.app().name);
 
 // const db = firebase.firestore(app);
