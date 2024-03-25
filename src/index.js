6756d7b1464528c73b869560b7da8f5381de8cb8
DeliriousEC <m-trankov@mail.ru>
2024-03-25 22:44:10 +0300

    Еще фикс

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MainPage } from './pages/main/MainPage.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { Header } from './components/header/header.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
