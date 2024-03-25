2234a6c1a6a2e4bb40d498315bc18ae77dd7ec4e
SanyaKontr <Bs13111@mail.ru>
2024-03-25 14:05:36 +0300

    profile page

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import {
 import {
   Login
 } from './pages/login/login';
+import { Profile } from './pages/profile/profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
   <React.StrictMode >
+  <Profile />
   <Register />
   <Login />
   </React.StrictMode>
