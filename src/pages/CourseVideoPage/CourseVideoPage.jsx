f37bfd4bfda7e644586bafa2fbedb7a2c806906a
Denis Vasilev <d89615022404@gmail.com>
2024-03-28 23:25:07 +0100

    добавил проверку отсутствия exercises

--- src/pages/CourseVideoPage/CourseVideoPage.jsx
+++ src/pages/CourseVideoPage/CourseVideoPage.jsx
@@ -10,8 +10,7 @@ export const CourseVideoPage = ({ courses, logOut }) =>{
   
   const { isAuth, id } = useAuth(); const params = useParams();
   const myCourse = courses.filter((course)=> course._id === params.courseId)[0];
-
-
+    console.log(courses);
   return (
     <>
       {courses.length > 0 && (
@@ -20,10 +19,12 @@ export const CourseVideoPage = ({ courses, logOut }) =>{
           <S.CourseTitle>Йога</S.CourseTitle>
           <S.CourseDescribtion> {myCourse.name} </S.CourseDescribtion>
           < VideoPlayer srcVideo={myCourse.video}/>
-          <S.ExercisesDetails>
-            < MyExercises listExercises={myCourse.exercises}/>
-            < ProgressExercises/>
-          </S.ExercisesDetails>
+          {myCourse.exercises && (
+            <S.ExercisesDetails>
+              < MyExercises listExercises={myCourse.exercises}/>
+              < ProgressExercises/>
+            </S.ExercisesDetails>
+          )}
         </>
       )}
       {(courses?.length ?? 0) === 0 && (
