e9a9c902785d37b8cf144d15648fc6e3913f5e41
Lana <lolhowko@gmail.com>
2024-03-29 12:40:48 +0300

    add OutOfService component

---
+++ src/components/OutOfService/OutOfService.jsx
@@ -0,0 +1,12 @@
+import * as S from '../OutOfService/OutOfServiceStyle'
+
+export function OutOfService() {
+  return (
+    <S.Container>
+      <S.Titles>
+        <S.HeaderTitle>Сайт временно не работает</S.HeaderTitle>
+        <S.HeaderDescription>Попробуйте зайти попозже.</S.HeaderDescription>
+      </S.Titles>
+    </S.Container>
+  )
+}
