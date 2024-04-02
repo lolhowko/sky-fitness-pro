0b89776d9a999bcb2af787c71e7762d665f69d5a
Lana <lolhowko@gmail.com>
2024-04-02 15:56:50 +0300

    add correct to styles + progress-counted

---
+++ src/components/WorkoutPage/progress-counted/progress-counted.jsx
@@ -0,0 +1,15 @@
+import * as S from './progress-counted.styles'
+
+export default function CountedProgress({ tittle, closeModal }) {
+  return (
+    <>
+      <S.ProgressOkConfirm>
+        <span className="close" onClick={closeModal}>
+          &times;
+        </span>
+        <S.ProgressOkConfirmTitle> {tittle}</S.ProgressOkConfirmTitle>
+        <S.ProgressOkImg src="/ok.svg" alt="ok" />
+      </S.ProgressOkConfirm>
+    </>
+  )
+}
