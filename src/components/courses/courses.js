8d1f5251dd0024cf4bfd1a8311c6c789dcad773e
SanyaKontr <Bs13111@mail.ru>
2024-04-07 01:01:00 +0300

    добавление стилей для главной

--- src/components/courses/courses.js
+++ src/components/courses/courses.js
@@ -29,6 +29,13 @@ export const Card1 = styled.img`
     border-radius: 30px;
 backdrop-filter: blur(29.04px);
 background: rgba(205, 205, 205, 0.1);
+&:hover { 
+    transform: scale(1.05); 
+    transition-duration: 0.5s; 
+    box-shadow: 1px 1px 30px 15px rgba(39, 26, 200, 0.5); 
+    &:active{ 
+        transform: scale(1.1); 
+    }
 `
 
 export const errorText = styled.div`
